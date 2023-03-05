8f233841d487f059898f59653e3c4c33094f3e46
RS121212 <79850763+RS121212@users.noreply.github.com>
2023-03-05 12:59:39 +0100

    added first 5 quiz questions

--- week2/ball/ball.js
+++ week2/ball/ball.js
@@ -15,12 +15,21 @@ function start() {
 
 function nextBoard() {
     // keep old ball values for the sake of efficient clearing of the old display
+    old.x = ball.x;
+    old.y = ball.y;
 
     // handle ball is hitting the bounds
+    if(ball.x >= 400) ball.x = 20;
+    if(ball.y >= 400) ball.y = 0;
     //   reverse direction
     //   lose some energy relative to the current inertia (only velocity varies)
 
     // calculate new position
+    ball.x = ball.x +20 ;
+    //ball.dx = ball.dx + 10;
+    ball.y = ball.y +10;
+    //ball.dy = ball.dy + 10;
+
     // calculate any changes in velocity due to gravitational pull or medium resistance
 
 
