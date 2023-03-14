5acea28687c8dad34d5817e180fbcc563dd9a9c8
RS121212 <79850763+RS121212@users.noreply.github.com>
2023-03-14 16:31:20 +0100

    modified week3

--- week3/lambda/lambda.js
+++ week3/lambda/lambda.js
@@ -1,6 +1,30 @@
+const id = x => x;
+const konst = x => y => x;
+const snd = x => y => y;
 
+//const T = truecase => falsecase => truecase;
+//const T = x => y => x;
+const T = konst;
+const F = snd;
 
+// Quiz Frage 7 - T und F sind schon da
 
+//const and = p => q => p( q (T) (F) ) ( q (F)  (F) )
+const and = p => q => p(q) (F)
+//                      p is T      p is F
+//const and = p => q => p(q)(p)
+
+//const or = p => q => p( q (T) (T) ) ( q (T)  (F) )
+//const or = p => q => p(T)(q)
+const or = p => q => p(p)(q);
+
+const Pair = x => y => getter => getter(x)(y);
+const firstname = konst;
+const lastname = snd;
+
+const Left      = x => f => g => f(x);
+const Right     = x => f => g => g(g);
+const either    = e => f => g => e(f)(g);
 // ----- special -----
 
 const Tuple = n => [
